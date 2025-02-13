66a9cbe75d9cf2e450e2666e95635c1279f74e0e
Rafael Pereira <rafaelsantospereira03@gmail.com>
2025-02-13 16:24:35 -0300

    update

--- src/pages/(cdc-para-empresas)/page.tsx
+++ src/pages/(cdc-para-empresas)/page.tsx
@@ -37,7 +37,7 @@ export function CreditoParaEmpresaPage() {
 
       <Header />
 
-      <section className="rounded-b-[3rem] bg-primary bg-[url(/backgrounds/banner-empresa.png)] bg-cover bg-center bg-no-repeat py-12 md:rounded-b-[5rem] lg:h-[calc(45rem-5rem)] lg:py-0">
+      <section className="rounded-b-[3rem] bg-primary bg-[url(/backgrounds/banner-empresa.png)] bg-cover bg-top bg-no-repeat py-12 md:rounded-b-[5rem] lg:h-[calc(45rem-5rem)] lg:py-0">
         <div className="relative h-full">
           <div className="container flex h-full w-full flex-col items-center gap-12 lg:flex-row">
             <div className="w-full flex-[1.2]">
